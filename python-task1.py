9208fd85c5ef148a48adba19be375e11a1d09d66
Mohamed-Ahmed1694 <116628728+Mohamed-Ahmed1694@users.noreply.github.com>
2023-01-08 16:44:01 +0200

    Create python-task1.py

---
+++ python-task1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from datetime import date
+
+def hello(name):
+          name = str(name)
+          now = pd.to_datetime(str(date.today())
+
+          return name + ' ' + str(now) + ' !'
+                               print(hello('hamza'))
+
+output: hamza 6-jan-2023
